b6a997ea464c9fe3ae4e3eeb544d5803a272b151
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-27 12:51:59 -0500

    Creacion de enviroment

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular2023-ca639',
+    appId: '1:932222907957:web:306ec5176b4f184a0f237c',
+    storageBucket: 'angular2023-ca639.appspot.com',
+    apiKey: 'AIzaSyBZnC24YBIV0hEDZoqpgr8PiJlf3Br8X0k',
+    authDomain: 'angular2023-ca639.firebaseapp.com',
+    messagingSenderId: '932222907957',
+  }
+
+}
